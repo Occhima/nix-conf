5bb3dbdc41a5a296d5f99a951367e407b756c859
Marco Occhialini <marcoocchialini@hotmail.com>
2024-12-15 16:19:52 -0300

    initial flake config

--- flake.nix
+++ flake.nix
@@ -2,21 +2,86 @@
   description = "My NixOS config";
 
   inputs = {
+
+    ## nix things
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
- #
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+
+    # ========= Development =========
+    #
+    # For managing this nix os config
+
+
+
     # ========= Utilities =========
     #
     # Declarative partitioning and formatting
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    disko =  {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # do I really need this?
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
 
   };
 
-  outputs = { self, nixpkgs, ... }: {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+  outputs = { self, nixpkgs, ... }@inputs:
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+     let
+      inherit (self) outputs;
+      inherit (nixpkgs) lib;
 
-  };
+      #
+      # ========= Architectures =========
+      #
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+      ];
+      in
+{
+#
+      # ========= Formatting =========
+      #
+      # Nix formatter available through 'nix fmt' https://nix-community.github.io/nixpkgs-fmt
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      # Pre-commit checks
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        import ./checks { inherit inputs system pkgs; }
+      );
+      #
+      # ========= DevShell =========
+      #
+      # Custom shell for bootstrapping on new hosts, modifying nix-config, and secrets management
+      devShells = forAllSystems (
+        system:
+        import ./shell.nix {
+          pkgs = nixpkgs.legacyPackages.${system};
+          checks = self.checks.${system};
+        }
+      );
+    };
+
+
+
 }
