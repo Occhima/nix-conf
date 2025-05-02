e8d0b35569fcf0b29d6181eee68fa77680005b99
occhima <marcoocchialini@usp.br>
2025-05-02 16:02:30 -0300

    feat: update spice theme

--- modules/home-manager/desktop/ui/themes/collection/guernica/targets/spicetify.nix
+++ modules/home-manager/desktop/ui/themes/collection/guernica/targets/spicetify.nix
@@ -13,10 +13,30 @@ let
 in
 {
   # Disable stylix integration for kitty conditionally
+  # NOTE: Stole from https://github.com/Gerg-L/nixos/blob/beeb8b6d907309d3ff10acc6c17a2aa0a2c235ad/nixosConfigurations/gerg-desktop/spicetify.nix#L4
   stylix.targets.spicetify.enable = lib.mkIf (cfg.enable && cfg.name == "guernica") false;
 
   # Override font and theme settings conditionally
   programs.spicetify = lib.mkIf (cfg.enable && cfg.name == "guernica") {
     theme = spicePkgs.themes.text;
+    colorScheme = "custom";
+    customColorScheme = {
+      text = "f8f8f8";
+      subtext = "f8f8f8";
+      sidebar-text = "79dac8";
+      main = "000000";
+      sidebar = "323437";
+      player = "000000";
+      card = "000000";
+      shadow = "000000";
+      selected-row = "7c8f8f";
+      button = "74b2ff";
+      button-active = "74b2ff";
+      button-disabled = "555169";
+      tab-active = "80a0ff";
+      notification = "80a0ff";
+      notification-error = "e2637f";
+      misc = "282a36";
+    };
   };
 }
