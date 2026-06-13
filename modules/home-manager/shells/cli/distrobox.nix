f7a4e30eeda7d65b97fec53c23c848ac2d6494cd
occhima <marcoocchialini@usp.br>
2026-06-13 18:39:40 -0300

    feat: update distro

--- modules/home-manager/shells/cli/distrobox.nix
+++ modules/home-manager/shells/cli/distrobox.nix
@@ -14,16 +14,44 @@ in
     programs.distrobox = {
       enable = true;
       enableSystemdUnit = true;
+
       containers = {
         netrunner = {
           image = "kalilinux/kali-rolling:latest";
           entry = true;
           home = "${config.home.homeDirectory}/pentesting/kali";
           init = true;
-          additional_packages = "git npm build-essential fish zsh neovim starship eza bat zoxide fzf seclists";
+          pre_init_hooks = "export SHELL=/usr/bin/nu;";
+          additional_packages = [
+            "git"
+            "npm"
+            "build-essential"
+            "fish"
+            "zsh"
+            "neovim"
+            "starship"
+            "eza"
+            "bat"
+            "zoxide"
+            "fzf"
+            "kali-linux-headless"
+            "golang"
+            "pipx"
+            "ca-certificates"
+            "curl"
+            "unzip"
+            "tar"
+          ];
+
+          init_hooks = [
+            "GOPATH=/root/go GOBIN=/usr/local/bin go install -v github.com/projectdiscovery/pdtm/cmd/pdtm@latest"
+            "/usr/local/bin/pdtm -install-all -bp /usr/local/bin"
+            "PIPX_HOME=/opt/pipx PIPX_BIN_DIR=/usr/local/bin pipx install pwncat-cs || true"
+          ];
         };
       };
     };
+
     home.packages = [ pkgs.distrobox-tui ];
   };
 }
